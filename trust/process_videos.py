1bdf4334fd89ce0514377fd77b9ed12b5dd94520
Pavlo Bazilinskyy <pavlo.bazilinskyy@gmail.com>
2023-06-15 15:54:21 +0200

    Cleanup of the ffmpeg command

--- trust/process_videos.py
+++ trust/process_videos.py
@@ -7,14 +7,12 @@ import pandas as pd
 df = pd.read_csv('process_videos_info.csv')
 
 for index, row in df.iterrows():
-    unity_file = '../public/videos/' + row['unity_file']
+    in_file = '../public/videos/' + row['unity_file']
     out_file = '../public/videos/compressed/' + row['video_file']
-    cmd = "ffmpeg -i " + unity_file + \
-          " -vcodec libx265 -vf " + \
-          "scale=1280:720 " + \
-          "-ss " + str(row['start']) + \
-          " -t " + str(row['end']) + \
-          " -crf 20 " + \
-          out_file
-    print(cmd)
-    os.system(cmd)
+    os.system("ffmpeg -i " + in_file +
+              " -vcodec libx265 -vf " +
+              "scale=1280:720 " +
+              "-ss " + str(row['start']) +
+              " -t " + str(row['end']) +
+              " -crf 20 " +
+              out_file)
