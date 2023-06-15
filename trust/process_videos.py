d81ef179178268337d241d126dea45a68c581bc6
Pavlo Bazilinskyy <pavlo.bazilinskyy@gmail.com>
2023-06-15 15:51:25 +0200

    ffmpeg command to process videos from unity

--- trust/process_videos.py
+++ trust/process_videos.py
@@ -1,8 +1,20 @@
+# by Pavlo Bazilinskyy <pavlo.bazilinskyy@gmail.com>
+
 import os
-for x in range(0, 21):
-    f1 = '../public/videos/video_' + str(x) + '.mp4'
-    out = '../public/videos/compressed/video_' + str(x) + '.mp4'
-    os.system("ffmpeg -i " + f1 +
-              " -vcodec libx265 -vf " +
-              "scale=1280:720 -crf 20 " +
-              out)
+import pandas as pd
+
+# read mapping csv
+df = pd.read_csv('process_videos_info.csv')
+
+for index, row in df.iterrows():
+    unity_file = '../public/videos/' + row['unity_file']
+    out_file = '../public/videos/compressed/' + row['video_file']
+    cmd = "ffmpeg -i " + unity_file + \
+          " -vcodec libx265 -vf " + \
+          "scale=1280:720 " + \
+          "-ss " + str(row['start']) + \
+          " -t " + str(row['end']) + \
+          " -crf 20 " + \
+          out_file
+    print(cmd)
+    os.system(cmd)
