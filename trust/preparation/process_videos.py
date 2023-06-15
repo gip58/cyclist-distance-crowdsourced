fda90f3dd7842fae0aeb5e3207344092ba2b65bd
Pavlo Bazilinskyy <pavlo.bazilinskyy@gmail.com>
2023-06-15 16:35:14 +0200

    Linting

--- trust/preparation/process_videos.py
+++ trust/preparation/process_videos.py
@@ -9,8 +9,8 @@ import trust as tr
 df = pd.read_csv('process_videos_info.csv')
 
 for index, row in df.iterrows():
-    in_file = os.path.join(tr.common.get_configs('source_video_files'), row['in'])
-    out_file = os.path.join(tr.settings.root_dir, 'public', 'videos', row['out'])
+    in_file = os.path.join(tr.common.get_configs('source_video_files'), row['in'])  # noqa: E501
+    out_file = os.path.join(tr.settings.root_dir, 'public', 'videos', row['out'])  # noqa: E501
     os.system("ffmpeg -i " + in_file +
               " -vcodec libx265 -vf " +
               "scale=1280:720 " +
