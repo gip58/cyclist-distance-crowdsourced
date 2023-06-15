d9a393d22f3fa08a4153c47ffda2801828fad2e3
Pavlo Bazilinskyy <pavlo.bazilinskyy@gmail.com>
2023-06-15 20:27:46 +0200

    Frame rate for ffmpeg command

--- trust/preparation/process_videos.py
+++ trust/preparation/process_videos.py
@@ -11,10 +11,11 @@ df = pd.read_csv('process_videos_info.csv')
 for index, row in df.iterrows():
     in_file = os.path.join(tr.common.get_configs('path_source'), row['in'])  # noqa: E501
     out_file = os.path.join(tr.common.get_configs('path_stimuli'), row['out'])  # noqa: E501
-    os.system("ffmpeg -i " + in_file +
-              " -vcodec libx265 -vf " +
-              "scale=1280:720 " +
-              "-ss " + str(row['start']) +
+    os.system("ffmpeg -r 60" +
+              " -i " + in_file +
+              " -vcodec libx265" +
+              " -vf scale=1280:720" +
+              " -ss " + str(row['start']) +
               " -t " + str(row['end']) +
               " -crf 20 " +
               out_file)
