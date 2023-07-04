47eb748cd6f08eaaf6e92dba3a35a9f3f4b0d123
Pavlo Bazilinskyy <pavlo.bazilinskyy@gmail.com>
2023-07-04 18:52:41 +0200

    Code for checking duration of videos

--- trust/preparation/process_videos.py
+++ trust/preparation/process_videos.py
@@ -11,11 +11,16 @@ df = pd.read_csv('process_videos_info.csv')
 for index, row in df.iterrows():
     in_file = os.path.join(tr.common.get_configs('path_source'), row['in'])  # noqa: E501
     out_file = os.path.join(tr.common.get_configs('path_stimuli'), row['out'])  # noqa: E501
-    os.system("ffmpeg -r 60" +
+    # using https://superuser.com/a/859075/52383
+    os.system("ffmpeg -r 30" +
+              " -ss " + str(row['start']) +
+              " -to " + str(row['end']) +
               " -i " + in_file +
               " -vcodec libx265" +
+              " -c:v libx264 -crf 23 -profile:v baseline -level 3.0 -pix_fmt yuv420p" +  # noqa: E501
+              " -c:a aac -ac 2 -b:a 128k" +
+              " -movflags faststart" +
               " -vf scale=1280:720" +
-              " -ss " + str(row['start']) +
-              " -t " + str(row['end']) +
+              " -an " +
               " -crf 20 " +
               out_file)
