ac3095efb831d265ba60a0321fb1a95970534284
Ruolin <107674677+Rolling0321@users.noreply.github.com>
2023-07-11 18:43:38 +0200

    Update process_videos.py
    make changes to compress the audio part from 64k to 32 k.

--- trust/preparation/process_videos.py
+++ trust/preparation/process_videos.py
@@ -4,23 +4,33 @@ import os
 import pandas as pd
 import trust as tr
 
-
-# read mapping csv
+# Read mapping csv
 df = pd.read_csv('process_videos_info.csv')
 
 for index, row in df.iterrows():
-    in_file = os.path.join(tr.common.get_configs('path_source'), row['in'])  # noqa: E501
-    out_file = os.path.join(tr.common.get_configs('path_stimuli'), row['out'])  # noqa: E501
-    # using https://superuser.com/a/859075/52383
-    os.system("ffmpeg -r 30" +
+    in_file = os.path.join(tr.common.get_configs('path_source'), row['in'])
+    out_file = os.path.join(tr.common.get_configs('path_stimuli'), row['out'])
+    temp_file = os.path.join(tr.common.get_configs('path_stimuli'), 'temp.mp4')
+    
+    # Using FFmpeg command to process video and compress audio
+    os.system("ffmpeg -r 60" +
               " -ss " + str(row['start']) +
               " -to " + str(row['end']) +
               " -i " + in_file +
-              " -vcodec libx265" +
-              " -c:v libx264 -crf 23 -profile:v baseline -level 3.0 -pix_fmt yuv420p" +  # noqa: E501
-              " -c:a aac -ac 2 -b:a 128k" +
+              " -c:v libx264 -crf 23 -profile:v baseline -level 3.0 -pix_fmt yuv420p" +
+              " -c:a aac -b:a 32k" +
+              " -vf scale=1080:720" +
               " -movflags faststart" +
-              " -vf scale=1280:720" +
-              " -an " +
-              " -crf 20 " +
+              " -crf 24 " +
+              temp_file)
+    
+    # Combine processed video and audio into a single output file
+    os.system("ffmpeg -i " + temp_file +
+              " -i " + in_file +
+              " -c:v copy -c:a copy" +
+              " -map 0:v:0 -map 1:a:0" +
+              " -shortest " +
               out_file)
+    
+    # Remove temporary file
+    os.remove(temp_file)
