f1027136c692e43513162a07cbc0c28cd04ecf07
Pavlo Bazilinskyy <pavlo.bazilinskyy@gmail.com>
2023-08-30 21:52:48 +0200

    Merging video and 1s of black
    not tested

--- trust/preparation/process_videos.py
+++ trust/preparation/process_videos.py
@@ -6,7 +6,10 @@ import trust as tr
 
 # Read mapping csv
 df = pd.read_csv('process_videos_info.csv')
-
+# black file to be added in the beginning of the stimuli
+black_file = os.path.join(tr.common.get_configs('path_source'),
+                          'black_video.mp4')
+# go over all stimuli
 for index, row in df.iterrows():
     in_file = os.path.join(tr.common.get_configs('path_source'), row['in'])
     out_file = os.path.join(tr.common.get_configs('path_stimuli'), row['out'])
@@ -20,4 +23,14 @@ for index, row in df.iterrows():
               " -vf scale=1280:720" +
               " -movflags faststart" +
               " -crf 24 " +
+              out_file)
+    # Using FFmpeg command to add 1 sec of black in the beginning
+    # # based on https://stackoverflow.com/a/22688066/46687
+    # os.system("ls " + black_file + " " + out_file + " |" +
+    #           "perl -ne 'print \"file $_\"' |"
+    #           "ffmpeg -f concat -i - -c copy " + out_file)
+    # based on https://stackoverflow.com/a/22958746/46687
+    os.system("ffmpeg -i " + black_file +
+              " -i " + out_file +
+              " -filter_complex \"concat=n=2:v=0:a=1\" -vn -y " +
               out_file)
