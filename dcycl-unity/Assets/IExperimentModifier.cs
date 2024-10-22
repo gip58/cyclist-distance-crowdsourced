53d9db33674497e6a43ed8aacaca8716458581df
Pavlo Bazilinskyy <pavlo.bazilinskyy@gmail.com>
2024-10-22 12:42:39 +0200

    Old unity file

--- dcycl-unity/Assets/IExperimentModifier.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IExperimentModifier {
-    public void SetParameter(NetworkingManager.ExperimentParameter[] experimentParameters);
-}
--- dcycl-unity/Assets/Scripts/LevelManager/LevelNames.cs
+++
@@ -1,7 +0,0 @@
-﻿public static class LevelNames
-{
-    public const string Menu = "MenuScene";
-    public const string Main = "MainScene";
-    public const string Test = "TestScene";
-    public const string Trial = "Trial";
-}
