a7b51a9cc29e6da9af69dbbc46bde633036ee402
gip58 <149475442+gip58@users.noreply.github.com>
2024-11-08 17:06:31 +0100

    delete unity

--- dcycl-unity/Assets/Scripts/Car/BlinkPatternData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "BlinkPatternData", menuName = "Coupled Simulator/BlinkPatternData")]
-public class BlinkPatternData : CustomBehaviourData
-{
-    public AnimationCurve blinkPattern;
-}
